ccc530e73b664e43cdf0669cae70957c49b98dca
Pablo <lozano.p15@gmail.com>
2023-07-23 02:02:04 +0200

    chore: clean up

--- init.lua
+++ init.lua
@@ -639,11 +639,9 @@ local hop = require('hop')
 local directions = require('hop.hint').HintDirection
 vim.keymap.set('n', 'f', function()
   hop.hint_words({ direction = directions.AFTER_CURSOR, current_line_only = false })
-  -- hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
 end, { remap = true })
-vim.keymap.set('', 'F', function()
+vim.keymap.set('n', 'F', function()
   hop.hint_words({ direction = directions.BEFORE_CURSOR, current_line_only = false })
-  -- hop.hint_anywhere({ direction = directions.BEFORE_CURSOR, current_line_only = false })
 end, { remap = true })
 
 
